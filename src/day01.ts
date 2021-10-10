e95773be3b9e38265311bf1f1f46f590ae30faeb
Markus Bauer <mkbauer@web.de>
2021-10-10 17:02:39 +0200

    Solution for day 01 part 2.

--- src/day01.ts
+++ src/day01.ts
@@ -6,6 +6,18 @@ function compute_fuel(m: number) {
     return Math.floor(m/3) - 2;
 }
 
+function compute_fuel_including_fuel(m: number) {
+    let fuel = 0
+    let mass_to_consider = m;
+    while (mass_to_consider > 0) {
+        mass_to_consider = compute_fuel(mass_to_consider);
+        // console.log(mass_to_consider)
+        if (mass_to_consider > 0)
+            fuel += mass_to_consider;
+    }
+    return fuel;
+}
+
 function part1(lines : string[]) {
     var result = 0;
     for (const line of lines) {
@@ -19,12 +31,27 @@ function part1(lines : string[]) {
     return result;
 }
 
+function part2(lines : string[]) {
+    var result = 0;
+    for (const line of lines) {
+        if (line) {
+            const mass = Number(line);
+            const fuel = compute_fuel_including_fuel(mass);
+            // console.log(`${mass} requires ${fuel}`);
+            result += fuel;
+        }
+    }
+    return result;
+}
+
 async function main() {
     const lines = await read_inputfile("input01.txt")
-    const result = part1(lines);
-    console.log(`The answer for part one is: ${result}`)    
+    const result1 = part1(lines);
+    console.log(`The answer for part one is: ${result1}`)  
+    const result2 = part2(lines);
+    console.log(`The answer for part one is: ${result2}`)  
 }
 
 main();
 
-export {compute_fuel };
+export {compute_fuel, compute_fuel_including_fuel };
--- test/day01_test.ts
+++ test/day01_test.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 import 'mocha';
-import { compute_fuel } from '../src/day01';
+import { compute_fuel, compute_fuel_including_fuel } from '../src/day01';
 
 
 suite("Day01", () => {
@@ -9,6 +9,12 @@ suite("Day01", () => {
         assert.equal(compute_fuel(12), 2);
         assert.equal(compute_fuel(1969), 654);
         assert.equal(compute_fuel(100756), 33583);
+    });
+
+    test("compute fuel for modules (including fuel) on example data", () => {
+        assert.equal(compute_fuel_including_fuel(12), 2);
+        assert.equal(compute_fuel_including_fuel(1969), 966);
+        assert.equal(compute_fuel_including_fuel(100756), 50346);
     });
 
   });
